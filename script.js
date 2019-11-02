4ee0e8c48d757c0ee2e224708d19507926fb2c91
VadimAfonievich <0711aiova@gmail.com>
2019-11-02 16:59:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+let money = +prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYY-MM-DD");
+
+let expense1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    expense01 = +prompt("Во сколько это обойдется?"),
+    expense2 = prompt("Введите обязательную статью расходов в этом месяце"),
+    expense02 = +prompt("Во сколько это обойдется?");
+
+let appData = {
+    finance: money,
+    timeData: time,
+    expenses: {
+        expense1: expense01,
+        expense2: expense02
+    }, 
+    optionalExpenses: "",
+    income: "",
+    savings: false
+};
+
+appData.expenses[expense1] = expense01;
+appData.expenses[expense2] = expense02;
+
+
+console.log(appData.expenses);
+alert("Ваш ежедневный бюджет составляет - " + ((appData.finance - (expense01 + expense02)) / 30) + " рублей");
+
+
+
+// console.log((money - (expense01 + expense02)) / 30);
+
+
+
+/*
+let answers = [],
+    qwestions = [
+        "Введите обязательную статью расходов в этом месяце",
+        "Во сколько это обойдется?"
+    ];
+
+for(let i = 0; i < qwestions.length; i++) {
+    answers[i] = prompt(qwestions[i], "");
+};
+for(let i = 0; i < qwestions.length; i++) {
+    answers[i] = prompt(qwestions[i], "");
+};
+*/
